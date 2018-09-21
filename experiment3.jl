4f4bb559c1a843370ca70fde8c16e9253d0dfabe
Roshan Shariff <roshan.shariff@ualberta.ca>
2018-09-21 00:13:54 -0600

    Run all three algs per run of experiment 3.

--- experiment3.jl
+++ experiment3.jl
@@ -1,14 +1,13 @@
 #!/usr/bin/env julia
-#SBATCH --time=02:30:00
+#SBATCH --time=02:55:00
 #SBATCH --mem-per-cpu=1000M
-#SBATCH --array=1-5760
+#SBATCH --array=1-1920
 #SBATCH --mail-user=rshariff@ualberta.ca
 #SBATCH --mail-type=ALL
 
 using JLD2
 using DataStructures
 using Printf
-using Base.Iterators: product
 
 using PrivateBandits.DifferentialPrivacy
 using PrivateBandits.LinearBandits
@@ -37,29 +36,25 @@ num_ρmins = 8
 ρmin_interp(c) = exp((1-c)*log(ρmin_lo) + c*log(ρmin_hi))
 ρmins = ρmin_interp.(((1:num_ρmins).-2) .// (num_ρmins-3))
 
-algs = OrderedDict{String, ContextLinBandit}()
-for ρmin in ρmins
-    algs["NonPrivate,ρmin=$ρmin"] =
-        make_alg(env, horizon; ρ=ρmin)
-    algs["Wishart,ρmin=$ρmin"] =
-        make_alg(env, horizon, shifted(WishartMechanism; ρmin=ρmin); dp...)
-    algs["Gaussian,ρmin=$ρmin"] =
-        make_alg(env, horizon, shifted(GaussianMechanism; ρmin=ρmin); dp...)
-end
-
-alg_names = collect(keys(algs))
-
 function task_params(A...; taskid=parse(Int, ENV["SLURM_ARRAY_TASK_ID"]))
     Tuple(CartesianIndices(((size.(A)...)..., taskid))[taskid])
 end
 
-(alg_ix, gap_ix, run_ix) = task_params(alg_names, gaps)
+(ρmin_ix, gap_ix, run_ix) = task_params(ρmins, gaps)
+ρmin = ρmins[ρmin_ix]
 gap = gaps[gap_ix]
-alg_name = alg_names[alg_ix]
+
+algs = OrderedDict{String, ContextLinBandit}(
+    "NonPrivate" => make_alg(env, horizon; ρ=ρmin),
+    "Wishart" => make_alg(env, horizon, shifted(WishartMechanism; ρmin=ρmin); dp...),
+    "Gaussian" => make_alg(env, horizon, shifted(GaussianMechanism; ρmin=ρmin); dp...)
+)
+
 arms = GapArms(env; gap=gap)
 
-result_name = "$alg_name,Δ=$gap"
-mkpath(result_name)
-
-@time result = run_episode(env, algs[alg_name], arms, horizon; subsample=10^4)
-@save joinpath(result_name, @sprintf("%03d.jld", run_ix)) result
+for (alg_name, alg) in algs
+    result_name = "$alg_name,ρmin=$ρmin,Δ=$gap"
+    mkpath(result_name)
+    @time result = run_episode(env, alg, arms, horizon; subsample=10^4)
+    @save joinpath(result_name, @sprintf("%03d.jld", run_ix)) result
+end
